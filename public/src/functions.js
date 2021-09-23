426e892d608d5c7138b7ab47b6ddfd844b1d6fce
AlejandroLaraMoncho <alejandrolaram243@gmail.com>
2021-09-23 15:35:59 +0200

    EjercicioNotasAlumnos

--- public/src/functions.js
+++ public/src/functions.js
@@ -1,28 +1,41 @@
 'use strict'
 
-function buscaAlumno() {
-	
+function buscaAlumno(datos, nia) {
+	return datos.find((item) => item.nia === nia)
 }
 
-function modulosAprobadosDelAlumno() {
+function modulosAprobadosDelAlumno(notas) {
+	return notas.filter((item) => item.nota >= 5).map((item) => item.modulo)
 }
 
-function media() {
+function media(notas) {
+	return ((notas.reduce((total,nota) => total += nota)) / notas.length).toFixed(2)
 }
 
-function ordenaAlumnosPorNia() {
+function ordenaAlumnosPorNia(datos) {
+	return datos.sort((item1,item2) => item1.nia - item2.nia)
 }
 
-function ordenaAlumnosPorApellido() {
+function ordenaAlumnosPorApellido(datos) {
+	return datos.sort((item1,item2) => item1.alumno.apellido.localeCompare(item2.alumno.apellido))
 }
 
-function alumnosConSuspensos() {
+function alumnosConSuspensos(datos) {
+	return datos.filter((item) => item.notas
+	.some((item2) => item2.nota < 5))
+	.map((item) => item.alumno.nombre + " " + item.alumno.apellido)
 }
 
-function alumnosConTodoAprobado() {
+function alumnosConTodoAprobado(datos) {
+	return datos.filter((item) => item.notas
+	.every((item2) => item2.nota >= 5))
+	.map((item) => item.alumno.nombre + " " + item.alumno.apellido)
 }
 
-function alumnosAprobadosDelModulo() {
+function alumnosAprobadosDelModulo(datos, modulo) {
+	return datos.filter((item) => item.notas
+	.find((item2) => item2.modulo == modulo && item2.nota >= 5))
+	.map((item) => item.alumno.nombre + " " + item.alumno.apellido)
 }
 
 
--- public/src/index.js
+++ public/src/index.js
@@ -1,2 +1,23 @@
 'use strict'
 
+
+let filtro = 123456//prompt("Introduce un NIA")
+
+let alumno = buscaAlumno(datosAlumnos,filtro)
+
+let modulosAprobados = modulosAprobadosDelAlumno(alumno.notas)
+
+let notasNumericas = []
+alumno.notas.forEach(element => notasNumericas.push(element.nota))
+
+let mediaDeAlumno = media(alumno.notas)
+
+let alumnosOrdenadosPorNia = ordenaAlumnosPorNia(datosAlumnos)
+
+let alumnosOrdenadosPorApellido = ordenaAlumnosPorApellido(datosAlumnos)
+
+let listaAlumnosConSuspensos = alumnosConSuspensos(datosAlumnos)
+
+let listaAlumnosTodoAprobado = alumnosConTodoAprobado(datosAlumnos)
+
+let listaAlumnosConModuloAprobado = alumnosAprobadosDelModulo(datosAlumnos, "dwec")
